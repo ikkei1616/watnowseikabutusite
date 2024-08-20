d1777e712a8f1d5da3feef82f8357ba4fcb3e058
Taichi <takezohuto1123@icloud.com>
2024-08-21 08:02:54 +0900

    イベント詳細ページでイベントを表示する際に必要な型を定義しました

--- src/types/Event.ts
+++ src/types/Event.ts
@@ -1,7 +1,16 @@
-//Eventの型定義
+//Event一覧ページで利用する型定義
 export interface Event {
     id: string; 
     name: string;
     date: string;
     comment: string;
+}
+
+//Event詳細ページで利用する型定義
+export interface EventDetail{
+    id: string
+    name: string
+    date: string
+    comment: string
+    url: string
 }
