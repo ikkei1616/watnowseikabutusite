f1e657846bb8180b7feb383c4df1e1f153f43b90
Taichi <takezohuto1123@icloud.com>
2024-08-20 23:35:55 +0900

    イベント一覧ページのイベントがsupabase_eventsから参照できるように変更しました

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+"use client"
+
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from './Page.module.css';
-import EventCard from '../../components/EventCard'; // Adjust the import path if necessary
-
-const mock_events = [
-  { id: '1', name: 'Watnowハッカソン2024', date: '2024-9', comment: 'みんな開発をいっぱい頑張りました！' },
-  { id: '2', name: 'Watnowハッカソン2024 春プロ', date: '2024-4', comment: 'みんな開発をいっぱい頑張りました！' },
-  { id: '3', name: 'Watnowハッカソン2024 秋プロ', date: '2024-9', comment: 'みんな開発をいっぱい頑張りました！' }
-];
+import EventCard from '../../components/EventCard'; 
+import { supabase } from '../../supabase/supabase';
 
 const EventPage: React.FC = () => {
+  const [events, setEvents] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data, error } = await supabase
+        .from('events')
+        .select('id, name, date, comment') 
+        .order('id', { ascending: true }); // 'id'で昇順にソート
+  
+      //取得に関するエラーハンドリング
+      if (error) {
+        console.error('Error fetching events:', error);
+      } else {
+        console.log('Fetched data:', data); // デバッグ用に取得データを出力
+        setEvents(data || []); // データがnullのときの対策として空配列を設定
+      }
+
+    };
+  
+    fetchEvents();
+  }, []);
+  
+
+
   return (
     <div className={styles.pageHeader}>
       <h1>これはイベント一覧ページです</h1>
-      {mock_events.map((event) => (
+      {events.map((event) => (       
         <EventCard key={event.id} event={event} />
        ))}
       <Link href="/events/new" className={styles.newEventButton}>
