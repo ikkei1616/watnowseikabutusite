cd501665ee41aed344eb345c13295634221b0591
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-23 00:17:03 +0900

    TextInputコンポーネントでregister,errorsの処理を追加

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import {useForm } from "react-hook-form";
 
 type TextInputProps = {
     label: string;
     id: string;
+    register: any;
+    errors?:any; 
 };
 
 const TextInput = ({
     label = "",
-    id = ""
+    id = "",
+    register,
+    errors
 }:TextInputProps): JSX.Element => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
-        <input type="text" id={id}/>
+        <input type="text" id={id} {...register(id)}/>
+        {errors && errors[id] && <p style={{color:"red"}}>{errors[id]?.message}</p>}
     </>
   );
 };
