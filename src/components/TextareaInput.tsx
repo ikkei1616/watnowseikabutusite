a1c19818e9725a3667fb349a738b6d2994b34317
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-23 15:45:25 +0900

    TextareaInutにreact-hook-formが使えるように更新

--- src/components/TextareaInput.tsx
+++ src/components/TextareaInput.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
 type TextareaInputProps = {
-    label: string;
-    id: string;
+  label: string;
+  id: string;
+  register: any;
+  errors?: any;
 };
 
 const TextareaInput = ({
-    label = "",
-    id = ""
-}:TextareaInputProps): JSX.Element => {
+  label = "",
+  id = "",
+  register,
+  errors
+}: TextareaInputProps): JSX.Element => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
-        <textarea id={id} />
+      <textarea id={id} {...register(id)} />
+      {errors && errors[id] && <p style={{ color: "red" }}>{errors[id]?.message}</p>}
     </>
   );
 };
