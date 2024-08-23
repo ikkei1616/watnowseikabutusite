1b0ace893fb8cbebfc9482b2a65f1565b3ab7fca
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-23 15:45:38 +0900

    NumberInputにreact-hook-formが使えるように更新

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -3,16 +3,21 @@ import React from 'react'
 type NumberInputProps = {
     label: string;
     id: string;
+    register: any;
+    errors?: any;
 };
 
 const NumberInput = ({
     label = "",
-    id = ""
+    id = "",
+    register,
+    errors
 }:NumberInputProps): JSX.Element => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
-        <input type="number" id={id}/>
+        <input type="number" id={id} {...register(id, { valueAsNumber: true })}/>
+        {errors && errors[id] && <p style={{color:"red"}}>{errors[id]?.message}</p>}
     </>
   );
 };
