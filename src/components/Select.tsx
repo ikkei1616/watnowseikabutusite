893b387e2b2b07324e9918d673c54369c25dd457
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-23 15:45:10 +0900

    Selectにreact-hook-formが使えるように更新

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -9,22 +9,26 @@ type SelectProps = {
     label: string;
     options: Option[];
     id: string;
+    register: any;
+    errors?: any;
 };
 
 const Select = ({
     id = "",
     label = "",
-    options = []
-
+    options = [],
+    register,
+    errors
 }:SelectProps): JSX.Element => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
-        <select id={id}>
+        <select id={id} {...register(id)}>
             {options.map((option) => (
                 <option value={option.value}>{option.label}</option>
             ))}
         </select>
+        {errors && errors[id] && <p style={{color:"red"}}>{errors[id]?.message}</p>}
     </>
   );
 };
