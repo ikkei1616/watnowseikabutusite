49bc67780d429e6b33a450745361ea59d8f66e63
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-25 17:46:34 +0900

    TextInputにFieldWrapperコンポーネント適用

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
-import {useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import  FieldWrapper  from './FieldWrapper';
 
 type TextInputProps = {
-    label: string;
-    id: string;
-    register: any;
-    errors?:any; 
+  label: string;
+  id: string;
+  register: any;
+  errors?: any;
 };
 
 const TextInput = ({
-    label = "",
-    id = "",
-    register,
-    errors
-}:TextInputProps): JSX.Element => {
+  label = "",
+  id = "",
+  register,
+  errors
+}: TextInputProps): JSX.Element => {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-        <input type="text" id={id} {...register(id)}/>
-        {errors && errors[id] && <p style={{color:"red"}}>{errors[id]?.message}</p>}
+      <FieldWrapper label={label} id={id} error={errors[id]}>
+        <input type="text" id={id} {...register(id)} />
+      </FieldWrapper>
     </>
   );
 };
