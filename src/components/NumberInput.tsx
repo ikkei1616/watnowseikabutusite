37729b582edc6b3e986387370424449a0cebe91c
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-25 17:47:16 +0900

    NumberInputにFieldWrapperコンポーネント適用

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import FieldWrapper from './FieldWrapper';
 
 type NumberInputProps = {
-    label: string;
-    id: string;
-    register: any;
-    errors?: any;
+  label: string;
+  id: string;
+  register: any;
+  errors?: any;
 };
 
 const NumberInput = ({
-    label = "",
-    id = "",
-    register,
-    errors
-}:NumberInputProps): JSX.Element => {
+  label = "",
+  id = "",
+  register,
+  errors
+}: NumberInputProps): JSX.Element => {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-        <input type="number" id={id} {...register(id, { valueAsNumber: true })}/>
-        {errors && errors[id] && <p style={{color:"red"}}>{errors[id]?.message}</p>}
+      <FieldWrapper label={label} id={id} error={errors[id]}>
+        <input type="number" id={id} {...register(id, { valueAsNumber: true })} />
+      </FieldWrapper>
     </>
   );
 };
