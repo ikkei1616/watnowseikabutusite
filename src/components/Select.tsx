a93c054f5ed11b073afb7ae5d83df3625da88147
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-25 17:46:59 +0900

    SelecttにFieldWrapperコンポーネント適用

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,34 +1,35 @@
 import React from 'react'
+import FieldWrapper from './FieldWrapper';
 
 type Option = {
-    label: string;
-    value: string;
+  label: string;
+  value: string;
 }
 
 type SelectProps = {
-    label: string;
-    options: Option[];
-    id: string;
-    register: any;
-    errors?: any;
+  label: string;
+  options: Option[];
+  id: string;
+  register: any;
+  errors?: any;
 };
 
 const Select = ({
-    id = "",
-    label = "",
-    options = [],
-    register,
-    errors
-}:SelectProps): JSX.Element => {
+  id = "",
+  label = "",
+  options = [],
+  register,
+  errors
+}: SelectProps): JSX.Element => {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
+      <FieldWrapper label={label} id={id} error={errors[id]}>
         <select id={id} {...register(id)}>
-            {options.map((option) => (
-                <option value={option.value}>{option.label}</option>
-            ))}
+          {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
         </select>
-        {errors && errors[id] && <p style={{color:"red"}}>{errors[id]?.message}</p>}
+      </FieldWrapper>
     </>
   );
 };
