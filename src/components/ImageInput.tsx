c3118d975501bcf00f9c99f5d4c04eb386046800
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-26 19:30:26 +0900

    ImageInputにMUIを適用

--- src/components/ImageInput.tsx
+++ src/components/ImageInput.tsx
@@ -1,32 +1,61 @@
-import React from 'react'
-import FieldWrapper from './FieldWrapper';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import FieldWrapper, { FieldWrapperProps } from './FieldWrapper';
+import {
+  FieldValues,
+  useController,
+  UseControllerProps,
+} from "react-hook-form";
 
-type ImageInputProps = {
-    label: string;
-    id: string;
-    register: any;
-    errors?: any;
-};
+export type ImageInputProps<T extends FieldValues> = {
+  control: any;
+  name: string;
+  label: string;
+} & Pick<FieldWrapperProps, "label">;
 
-const ImageInput = ({
-    label = "",
-    id = "",
-    register,
-    errors
-}: ImageInputProps): JSX.Element => {
-    return (
-        <>
-            <FieldWrapper label={label} id={id} error={errors[id]}>
-                <input
-                    type="file"
-                    accept='.png, .jpg'
-                    multiple
-                    id={id}
-                    {...register(id)}
-                />
-            </FieldWrapper>
-        </>
-    );
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+const ImageInput = <T extends FieldValues>({
+  label,
+  ...props
+}: ImageInputProps<T>): JSX.Element => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(props);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      field.onChange(files); // ファイルをフィールドのonChangeに渡す
+    }
+  };
+
+  return (
+    <>
+      <FieldWrapper label={label} errorMessage={error?.message}>
+        <Button component="label" role={undefined} variant="contained" tabIndex={-1}>
+          Upload file
+          <VisuallyHiddenInput
+            type="file"
+            accept="image/*" 
+            onChange={handleFileChange} // ファイルが選択されたときに呼ばれるハンドラ
+          />
+        </Button>
+      </FieldWrapper>
+    </>
+  );
 };
 
 export default ImageInput;
