f304432c9499806159d61e829c6a4dd7c8032a5d
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-08-27 10:56:39 +0900

    仮ページ更新

--- src/app/services/new/page.tsx
+++ src/app/services/new/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 import React from 'react';
-import TextInput from '@/components/TextInput';
-import TextareaInput from '@/components/TextareaInput';
-import ImageInput from '@/components/ImageInput';
-import NumberInput from '@/components/NumberInput';
-import Select from '@/components/Select';
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ServiceInputSchema, ServiceOutputSchema, resolver } from "./serviceFormSchema"
-import { event, period } from "@/constants"
+import { useFormFields } from "./hooks";
+import { FormFactory } from "@/components/FormFactory";
 
 const NewServicesPage = () => {
   const { control, handleSubmit } = useForm<ServiceInputSchema>({
@@ -28,32 +24,11 @@ const NewServicesPage = () => {
           const d = (_d as unknown) as ServiceOutputSchema;
           onSubmit(d);
         })}>
-          <div>
-            <TextInput control={control} name="serviceName" label='サービス名'  />
-          </div>
-           <div>
-            <TextareaInput control={control} name="serviceInfo" label='サービス情報'  />
-          </div>
-          <div>
-            <TextInput control={control} name="teamName" label='チーム名' />
-          </div>
-          
-          <div>
-            <ImageInput control={control} name="serviceImage" label='画像' />
-          </div>
-          <div>
-            <TextInput control={control} name="award" label='賞' />
-          </div>
-          <div>
-            <Select control={control} name="selectEvent" label='イベント' options={event} />
-          </div>
-          <div>
-            <TextareaInput control={control} name="serviceDetail" label='サービス詳細情報' />
-          </div>
-          <div>
-            <NumberInput control={control} name="developmentPeriodNum" label='開発期間' />
-            <Select control={control} name="developmentPeriod" label='' options={period} /> 
-          </div>
+          {
+          useFormFields(control).map((field) => (
+            <FormFactory<ServiceInputSchema> {...field} />
+          ))
+        }
           <button type="submit">送信</button>
         </form>
       </main>
