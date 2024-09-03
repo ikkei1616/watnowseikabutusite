fc3859305c3e8e9fd1945a1241a4ad5038f6ab19
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2024-09-03 21:41:16 +1000

    henkou3

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import Link from 'next/link';
-import styles from './Page.module.css';
+import React from "react";
+import Link from "next/link";
+import styles from "./Page.module.css";
 
-const Home: React.FC = () =>{
-  const events = [
-    {id: "1",name:"event1"},
-    {id:"2",name:"event2"},
-    {id:"3",name:"event3"},
-    {id:"4",name:"event4"},
-    {id:"5",name:"event5"},
+const Home: React.FC = () => {
+  const services = [
+    { id: "1", name: "service1" },
+    { id: "2", name: "service2" },
+    { id: "3", name: "service3" },
+    { id: "4", name: "service4" },
+    { id: "5", name: "service5" },
   ];
-  return(
-    <div className ={styles.pageHeader}>
+  return (
+    <div className={styles.pageHeader}>
       <h1>これはサービス一覧ページです。</h1>
-      <ul className={styles.eventList}>
-        {events.map((event,num)=>(
-          <li key = {event.id} className= {styles.eventItem}>
-            <Link rel="stylesheet" href="/services/serviceid">
-              イベント{num+1}
-            </Link>
+      <ul className={styles.serviceList}>
+        {services.map((service, num) => (
+          <li key={service.id} className={styles.serviceItem}>
+            <Link href="/services/serviceid">サービス{num + 1}</Link>
           </li>
         ))}
       </ul>
     </div>
   );
-
 };
 
 export default Home;
