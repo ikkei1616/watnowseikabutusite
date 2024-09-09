98dc4fb9931563745dca6221e7feeae39bc4aa58
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-09 14:29:01 +0900

    schema更新

--- src/app/services/new/serviceFormSchema.ts
+++ src/app/services/new/serviceFormSchema.ts
@@ -2,24 +2,22 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
 const serviceSchema = z.object({
-    serviceName: z.string().min(1,"サービス名は必須です"),
-    serviceInfo: z.string(),
-    teamName: z.string(),
-    serviceImage: z.nullable(z.instanceof(FileList)),
-    award: z.string(),
-    selectEvent: z.string(),
-    serviceDetail: z.string(),
-    developmentPeriodNum: z.number({invalid_type_error: "数値を入力してください"}).positive().int(),
-    developmentPeriod: z.string(),
-})
-.transform(x =>{
-  const image = x.serviceImage?.item(0);
+  serviceName: z.string().min(1, "サービス名は必須です"),
+  serviceInfo: z.string(),
+  teamName: z.string(),
+  serviceImage: z.unknown(),
+  award: z.string(),
+  selectEvent: z.string(),
+  serviceDetail: z.string(),
+  developmentPeriodNum: z.number({ invalid_type_error: "数値を入力してください" }).positive().int(),
+  developmentPeriod: z.string(),
+}).transform((x) => {
+  const image = (x.serviceImage as FileList)?.item(0);
   return {
     ...x,
     image,
-  }
-
-})
+  };
+});
 
 export type ServiceInputSchema = z.input<typeof serviceSchema>;
 export type ServiceOutputSchema = z.output<typeof serviceSchema>;
