88422c564e605cc9f8bcb457ae12fbd426ff106a
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-09 22:29:16 +0900

    変更してしまって戻し忘れたファイルを修正

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,24 +11,9 @@ const Home: React.FC = () => {
     { id: "3", name: "service3" },
     { id: "4", name: "service4" },
     { id: "5", name: "service5" },
-    { id: "6", name: "service6" },
-    { id: "7", name: "service7" },
-    { id: "8", name: "service8" },
-    { id: "9", name: "service9" },
-    { id: "10", name: "service10" },
-    { id: "11", name: "service11" },
-    { id: "12", name: "service12" },
-    { id: "13", name: "service13" },
-    { id: "14", name: "service14" },
-    { id: "15", name: "service15" },
-    { id: "16", name: "service16" },
-    { id: "17", name: "service17" },
-    { id: "18", name: "service18" },
-    { id: "19", name: "service19" },
-    { id: "20", name: "service20" },
   ];
   return (
-    <main>
+    <div className={styles.pageHeader}>
       <Header mode={HeaderMode.SERVICES} />
 
       <ul className={styles.serviceList}>
@@ -38,7 +23,7 @@ const Home: React.FC = () => {
           </li>
         ))}
       </ul>
-    </main>
+    </div>
   );
 };
 
