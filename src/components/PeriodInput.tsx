f9869bd62916c49b115fae7acf2f282044ffd6cf
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-11 22:24:38 +0900

    PeriodInput作成

---
+++ src/components/PeriodInput.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import SelectInput, { Option } from './Select';
+import NumberInput from './NumberInput'; 
+import { FieldValues, Control, Path } from 'react-hook-form'; 
+
+export type PeriodProps<T extends FieldValues> = {
+    control: Control<T>; 
+    name: [Path<T>, Path<T>]; // Ensure name is a tuple of Path<T>
+    label: string;
+    options: Option[];
+};
+
+const Period = <T extends FieldValues>({
+    control,
+    name,
+    label,
+    options,
+}: PeriodProps<T>): JSX.Element => {
+    return (
+        <div style={{ display: 'flex', justifyContent: "flex-start", gap:"12px" }}>
+            <NumberInput
+                control={control}
+                name={name[0]} // Ensure this matches Path<T>
+                label={label}
+            />
+            <SelectInput
+                control={control}
+                name={name[1]} // Ensure this matches Path<T>
+                label={"　"}
+                options={options}
+                ending="間"
+            />
+        </div>
+    );
+}
+
+export default Period;
