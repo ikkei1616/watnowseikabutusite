c70af786eccb58c268cb2be2efba8b3ed16c3219
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-11 22:20:05 +0900

    TextareaInputスタイル調整

--- src/components/TextareaInput.tsx
+++ src/components/TextareaInput.tsx
@@ -6,6 +6,7 @@ import {
   useController,
   UseControllerProps,
 } from 'react-hook-form';
+import { Box } from '@mui/material';
 
 export type TextareaInputProps<T extends FieldValues> = {
   control: any;
@@ -24,13 +25,36 @@ const TextareaInput = <T extends FieldValues>({
 
   return (
     <>
-      <FieldWrapper label={label} errorMessage={error?.message}>
-        <TextareaAutosize
-          {...field}
-          minRows={3}
-          style={{ width: '100%' }}
-        />
-      </FieldWrapper>
+      <Box
+        sx={{
+          width: '100%',
+          '& .custom-textarea': {
+            fontSize: '1rem',
+            padding: '8px', 
+            border: '1px solid #9CABC7', 
+            borderRadius: '4px',
+            backgroundColor: 'rgba(240, 244, 248, 0)', 
+            boxSizing: 'border-box', 
+            outline: 'none',
+            '&:focus': {
+              borderColor: '#85D5F3',
+               borderWidth: '2px'
+            },
+            '&:hover': {
+              borderColor: '#85D5F3', 
+            },
+          },
+        }}
+      >
+        <FieldWrapper label={label} errorMessage={error?.message}>
+          <TextareaAutosize
+            {...field}
+            minRows={4}
+            className="custom-textarea"
+            style={{ width: '100%' }}
+          />
+        </FieldWrapper>
+      </Box>
     </>
   );
 };
