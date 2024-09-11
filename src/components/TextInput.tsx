ab500f12282389322a92f7e66830d6bdb533e11d
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-11 22:19:40 +0900

    TextInputスタイル調整

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -4,10 +4,10 @@ import FieldWrapper from './FieldWrapper';
 import { FieldValues, useController, Control, Path, PathValue } from 'react-hook-form';
 
 export type TextInputProps<T extends FieldValues> = {
-    control: Control<T>;
-    name: Path<T>;
-    label: string;
-    defaultValue?: PathValue<T, Path<T>>;
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  defaultValue?: PathValue<T, Path<T>>;
 }
 
 const TextInput = <T extends FieldValues>({
@@ -23,7 +23,36 @@ const TextInput = <T extends FieldValues>({
 
   return (
     <FieldWrapper label={label} errorMessage={error?.message}>
-      <TextField {...field} />
+<TextField
+  {...field}
+  sx={{
+    width: '100%',
+    fontSize: '1rem',
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#9CABC7',
+        borderRadius: '8px',
+      },
+      '&:hover fieldset': {
+        borderColor: '#85D5F3', 
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#85D5F3',
+      },
+    },
+  }}
+  InputProps={{
+    sx: {
+      padding: '4px 4px',
+      '& input': {
+        padding: '4px 4px',
+      },
+    },
+  }}
+  variant="outlined"
+/>
+
+
     </FieldWrapper>
   );
 };
