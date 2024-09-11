a07df2db0258304b3aed4f8c41a9fe207ac9804c
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-11 22:20:49 +0900

    Selectスタイル調整 valueがnumber型にも対応

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,16 +3,17 @@ import { FieldValues, useController, Control, Path } from 'react-hook-form';
 import { Select, MenuItem } from '@mui/material';
 import FieldWrapper from './FieldWrapper';
 
-type Option = {
+export type Option = {
   label: string;
-  value: string;
+  value: string | number;
 };
 
 export type SelectProps<T extends FieldValues> = {
   control: Control<T>;
   name: Path<T>;
-  label: string;
+  label?: string;
   options: Option[];
+  ending?: string;
 };
 
 const SelectInput = <T extends FieldValues>({
@@ -20,6 +21,7 @@ const SelectInput = <T extends FieldValues>({
   options,
   control,
   name,
+  ending
 }: SelectProps<T>): JSX.Element => {
   const {
     field: { value, onChange, onBlur, ref },
@@ -27,20 +29,52 @@ const SelectInput = <T extends FieldValues>({
   } = useController({ name, control });
 
   return (
-    <FieldWrapper label={label} errorMessage={error?.message}>
-      <Select
-        value={value || ''}
-        onChange={onChange}
-        onBlur={onBlur}
-        inputRef={ref}
-      >
-        {options.map(({ label, value }) => (
-          <MenuItem key={value} value={value}>
-            {label}
-          </MenuItem>
-        ))}
-      </Select>
-    </FieldWrapper>
+    <div style={{
+      display:"flex",
+    }}>
+      <FieldWrapper label={label} errorMessage={error?.message}>
+        <Select
+          value={value || ''}
+          onChange={onChange}
+          onBlur={onBlur}
+          inputRef={ref}
+          sx={{
+            minWidth: "100px",
+            '& .MuiSelect-select': {
+              padding: '8px',
+              boxSizing: 'border-box',
+            },
+            '& .MuiOutlinedInput-notchedOutline': {
+              borderColor: '#9CABC7',
+            },
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+              borderColor: '#85D5F3',
+            },
+            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+              borderColor: '#85D5F3',
+              borderWidth: '2px',
+            },
+          }}
+        >
+          {options.map(({ label, value }) => (
+            <MenuItem key={value} value={value}>
+              {label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FieldWrapper>
+      {ending && (
+        <span style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '1rem',
+          marginTop: '12px',
+          marginLeft: '4px',
+        }}>{ending}</span>
+      )
+      }
+    </div>
   );
 };
 
