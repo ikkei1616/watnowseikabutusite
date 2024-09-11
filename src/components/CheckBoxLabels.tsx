5cb5c52fe23f16a8fe37f35e1b575f5bdffed8c2
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-09-12 00:36:12 +0900

    Checkboxコンポーネント作成

---
+++ src/components/CheckBoxLabels.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { FieldValues, useController, Control, Path } from 'react-hook-form';
+
+export type CheckboxLabelsProps<T extends FieldValues> = {
+    control: Control<T>;
+    name: Path<T>;
+    label: string;
+  };
+
+const CheckboxLabels = <T extends FieldValues>({
+    control,
+    name,
+    label,
+  }: CheckboxLabelsProps<T>): JSX.Element => {
+    const {
+        field,
+        fieldState: { error },
+      } = useController({ name, control });
+    
+      return (
+        <FormGroup sx={{
+          margin: "12px 0",
+        }}>
+          <FormControlLabel control={<Checkbox {...field} />} label={label} />
+        </FormGroup>
+      );
+}
+
+export default CheckboxLabels;
