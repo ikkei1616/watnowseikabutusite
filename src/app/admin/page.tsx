1ca08bfb15d5a7fc25827f83e97af76c8344cd5a
YukaChoco <sugar.ore.yk@gmail.com>
2024-10-06 20:14:16 +0900

    adminページのデザイン実装

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,38 +2,48 @@ import Link from "next/link";
 import styles from "./page.module.css";
 import AdminHeader from "@/components/AdminHeader";
 import AdminPankuzuList from "@/components/AdminPankuzuList";
+import Typography from "@mui/material/Typography";
 
 export default function AdminPage() {
-  const pankuzu = [
-    { text: "ジャンル選択", link: "/admin" },
-  ];
+  const pankuzu = [{ text: "ジャンル選択", link: "/admin" }];
   return (
-    <main className={styles.container}>
+    <>
       <AdminHeader />
-      <AdminPankuzuList pankuzu={pankuzu} />
-      <h1 className={styles.heading}>ジャンル選択</h1>
-      <ul className={styles.linkList}>
-        <li className={styles.listItem}>
-          <Link href="/admin/events" className={styles.link}>
-            イベント
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/admin/services" className={styles.link}>
-            サービス
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/admin/technologies" className={styles.link}>
-            技術スタック
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/admin/users" className={styles.link}>
-            ユーザ
-          </Link>
-        </li>
-      </ul>
-    </main>
+      <main className={styles.container}>
+        <AdminPankuzuList pankuzu={pankuzu} />
+        <Typography
+          component="div"
+          sx={{
+            fontFamily: "HannariMincho",
+            fontSize: "4rem",
+            padding: "1rem 0",
+          }}
+        >
+          ジャンル選択
+        </Typography>
+        <ul className={styles.linkList}>
+          <li className={styles.listItem}>
+            <Link href="/admin/events" className={styles.link}>
+              イベント
+            </Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link href="/admin/services" className={styles.link}>
+              サービス
+            </Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link href="/admin/technologies" className={styles.link}>
+              技術スタック
+            </Link>
+          </li>
+          <li className={styles.listItem}>
+            <Link href="/admin/users" className={styles.link}>
+              ユーザ
+            </Link>
+          </li>
+        </ul>
+      </main>
+    </>
   );
 }
