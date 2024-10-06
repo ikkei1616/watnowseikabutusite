eb36fda1c401bc0ae760f1833c48578c2350e676
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2024-10-06 14:29:54 +0900

    DBからの名前とidの取得

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,29 +1,45 @@
-import React from "react";
+"use client";
+
+import React, {useEffect, useState} from "react";
 import Link from "next/link";
 import styles from "./Page.module.css";
 import Header from "@/components/Header";
 import { HeaderMode } from "@/types/HeaderMode";
+import { supabase } from "@/supabase/supabase";
+import type {Service} from "@/types/Service";
 
 const Home: React.FC = () => {
-  const services = [
-    { id: "1", name: "service1" },
-    { id: "2", name: "service2" },
-    { id: "3", name: "service3" },
-    { id: "4", name: "service4" },
-    { id: "5", name: "service5" },
-  ];
-  return (
-    <div className={styles.pageHeader}>
-      <Header mode={HeaderMode.SERVICES} />
+  const [services,setServices] = useState<Service[]>([]);
 
+  useEffect(() => {
+    const fetchService = async () => {
+      const {data,error} = await supabase
+        .from("services")
+        .select("id,name")
+        .order("id", {ascending: true});
+      
+      if(error) {
+        console.error("Error fetching events:", error);
+      } else {
+        console.log("Fetched data:", data);
+        setServices((data as Service[]) || []);
+      }
+    };
+
+    fetchService(); 
+  },[]);
+
+  return (
+    <main>
+      <Header mode={HeaderMode.SERVICES} />
       <ul className={styles.serviceList}>
         {services.map((service, num) => (
           <li key={service.id} className={styles.serviceItem}>
-            <Link href="/services/serviceid">サービス{num + 1}</Link>
+            <Link href="/services/serviceid">{service.name}{num + 1}</Link>
           </li>
         ))}
       </ul>
-    </div>
+    </main>
   );
 };
 
