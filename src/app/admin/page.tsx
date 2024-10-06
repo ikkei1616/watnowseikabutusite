34c509fcf7689be263d3507fb4a858899aa34a88
YukaChoco <sugar.ore.yk@gmail.com>
2024-10-06 19:20:30 +0900

    AdminHeaderの使用

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import AdminHeader from "@/components/AdminHeader";
 
 export default function AdminPage() {
   return (
     <div className={styles.container}>
+      <AdminHeader />
       <h1 className={styles.heading}>ジャンル選択</h1>
       <ul className={styles.linkList}>
         <li className={styles.listItem}>
