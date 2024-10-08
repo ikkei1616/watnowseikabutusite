e7944e6ee54a6515226fd46f8454f892a17affe6
YukaChoco <sugar.ore.yk@gmail.com>
2024-10-08 18:13:39 +0900

    消した

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,16 +6,6 @@ export default function AdminPage() {
     <div className={styles.container}>
       <h1 className={styles.heading}>ジャンル選択</h1>
       <ul className={styles.linkList}>
-        <li className={styles.listItem}>
-          <Link href="/admin/events" className={styles.link}>
-            イベント
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/admin/services" className={styles.link}>
-            サービス
-          </Link>
-        </li>
       </ul>
     </div>
   );
