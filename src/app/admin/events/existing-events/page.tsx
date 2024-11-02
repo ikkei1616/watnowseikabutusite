8a88a2ddb306c85defd34091b3eb74c5a42730f4
YukaChoco <sugar.ore.yk@gmail.com>
2024-11-02 19:48:56 +0900

    EventPageでも年毎で選択できるように変更

--- src/app/admin/events/existing-events/page.tsx
+++ src/app/admin/events/existing-events/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "../../admin.module.css";
-import { supabase } from "@/supabase/supabase";
-import type { Event } from "@/types/Event";
+
 import List from "@mui/material/List";
 import Box from "@mui/material/Box";
 import AdminHeader from "@/components/admin/AdminHeader";
@@ -11,12 +10,14 @@ import PankuzuList from "@/components/admin/PankuzuList";
 import AdminTitle from "@/components/admin/AdminTitle";
 import AdminExistingLinkItem from "@/components/admin/AdminExistingLinkItem";
 import SelectYearOptions from "@/components/admin/SelectYearOptions";
+import DisplayError from "@/components/admin/DisplayError";
 import useAdminSelectYear from "@/hooks/useAdminSelectYear";
+import useAdminEventList from "@/hooks/useAdminEventList";
 import { YEARS_OPTIONS } from "@/const";
 
 const EventPage: React.FC = () => {
-  const [events, setEvents] = useState<Event[]>([]);
   const { selectedYear, handleYearChange } = useAdminSelectYear(YEARS_OPTIONS);
+  const { events, error } = useAdminEventList(selectedYear);
 
   const pankuzu = [
     { text: "ジャンル選択", link: "/admin" },
@@ -24,24 +25,17 @@ const EventPage: React.FC = () => {
     { text: "既存ページ編集", link: "/admin/events/existing-events" },
   ];
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const { data, error } = await supabase
-        .from("events")
-        .select("id, name, date, comment")
-        .order("id", { ascending: true }); // 'id'で昇順にソート
-
-      //取得に関するエラーハンドリング
-      if (error) {
-        console.error("Error fetching events:", error);
-      } else {
-        console.log("Fetched data:", data); // デバッグ用に取得データを出力
-        setEvents((data as Event[]) || []); // データがnullのときの対策として空配列を設定
-      }
-    };
-
-    fetchEvents();
-  }, []);
+  if (error) {
+    return (
+      <>
+        <AdminHeader />
+        <main className={styles.container}>
+          <PankuzuList pankuzu={pankuzu} />
+          <DisplayError height={"calc(100vh - 200px)"} error={error} />
+        </main>
+      </>
+    );
+  }
 
   return (
     <>
@@ -59,14 +53,27 @@ const EventPage: React.FC = () => {
           <Box sx={{ flexGrow: 1 }}>
             <AdminTitle>{selectedYear}年のイベント一覧</AdminTitle>
             <List>
-              {events.map((event) => (
-                <AdminExistingLinkItem
-                  key={event.id}
-                  href={`./existing-events/${event.id}/edit`}
-                >
-                  {event.name}
-                </AdminExistingLinkItem>
-              ))}
+              {events &&
+                (events.length !== 0 ? (
+                  events.map((event) => (
+                    <AdminExistingLinkItem
+                      key={event.id}
+                      href={`./existing-events/${event.id}/edit`}
+                    >
+                      {event.name}
+                    </AdminExistingLinkItem>
+                  ))
+                ) : (
+                  <p
+                    style={{
+                      textAlign: "center",
+                      fontSize: "1.5rem",
+                      marginTop: "40px",
+                    }}
+                  >
+                    データがありません
+                  </p>
+                ))}
             </List>
           </Box>
         </Box>
