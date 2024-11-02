578a555ac790a6f9228eaea1238ea59d551f57f8
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-11-02 17:41:19 +0900

    技術登録ページ作成

---
+++ src/app/admin/technologies/new/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React from 'react';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ServiceInputSchema, ServiceOutputSchema, resolver } from "./technologiesFormSchema";
+import { useFormFields } from "./hooks";
+import { FormFactory } from "@/components/form/FormFactory";
+import FormButton from '@/components/form/FormButton';
+import { supabase } from '@/supabase/supabase';
+import AdminHeader from '@/components/admin/AdminHeader';
+
+const NewServicesPage = () => {
+  const { control, handleSubmit } = useForm<ServiceInputSchema>({
+    mode: "onChange",
+    resolver: resolver,
+  });
+
+  const [isRoading, setIsLoading] = React.useState(false);
+
+  const onSubmit: SubmitHandler<ServiceOutputSchema> = async (data) => {
+    setIsLoading(true);
+
+    let imageUrl = '';
+
+    if (data.iconImage instanceof File) {
+      const imageFileName = encodeURIComponent(`${Date.now()}-${data.iconImage.name.replace(/[^a-zA-Z0-9.]/g, '_')}`);
+      const { error: uploadError } = await supabase.storage
+        .from('technology_icons')
+        .upload(imageFileName, data.iconImage);
+
+      if (uploadError) {
+        console.error('Error uploading file: ', uploadError.message);
+        return;
+      }
+
+      const { data: publicImageUrlData } = await supabase.storage
+        .from('technology_icons')
+        .getPublicUrl(imageFileName);
+
+      imageUrl = publicImageUrlData.publicUrl || '';
+    }
+
+    const { iconImage, ...rest } = data;
+    const submitData = { ...rest, image: imageUrl };
+
+    const { error: insertError } = await supabase
+      .from('technologies')
+      .insert([submitData])
+      .select();
+
+    if (insertError) {
+      console.error('Error inserting service:', insertError);
+      return;
+    }
+
+    window.location.href = '/admin/services/existing-services';
+  };
+
+  const formFields = useFormFields(control);
+
+  if (isRoading) {
+    return <div>ローディング中...</div>;
+  }
+
+  return (
+    <>
+      <main style={{
+        width: "90%",
+        margin: "0 auto",
+      }
+      }>
+        <AdminHeader />
+        <h1 style={{
+          borderBottom: "1px solid #9CABC7",
+          paddingBottom: "12px",
+          marginBottom: "12px",
+        }}>新規技術スタック作成</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {formFields.map(({ title, fields }, index) => (
+            <section key={index} style={{
+              borderBottom: "1px solid #9CABC7",
+              paddingBottom: "12px",
+              marginBottom: "12px",
+            }}>
+              <h3>{title}</h3>
+              {fields.map((field) => (
+                <FormFactory<ServiceInputSchema> key={field.id} {...field} />
+              ))}
+            </section>
+          ))}
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '60px',
+            margin: "20px 0"
+          }}>
+            <FormButton name="キャンセル" type='cancel' onClick={() => window.location.href = '/admin/services'}/>
+            <FormButton name="新規作成" type='submit' />
+          </div>
+        </form>
+      </main>
+    </>
+  );
+};
+
+export default NewServicesPage;
