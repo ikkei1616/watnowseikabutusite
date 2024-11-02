adedef4bfdb092265a9477e6eedc6097e105f06c
chiiiihc <is0710kf@ed.ritsumei.ac.jp>
2024-11-02 12:36:02 +0900

    サービス詳細を表示させる

--- src/app/services/[service_id]/page.tsx
+++ src/app/services/[service_id]/page.tsx
@@ -18,6 +18,7 @@ export default function ServiceDetailPage({
   const serviceID = params.service_id;
 
   const [service, setService] = useState<ServiceDetail | null>(null);
+  const [relatedEventName, setRelatedEventName] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
@@ -40,16 +41,26 @@ export default function ServiceDetailPage({
           );
           setService(null); // サービスデータがない場合はnullに設定
         } else {
-          setService({
-            ...serviceData,
-            // awards: awardsData || [],
-          } as ServiceDetail);
+          setService(serviceData as ServiceDetail);
+
+          // 関連イベントの名前を取得
+          const { data: eventData, error: eventError } = await supabase
+            .from("events")
+            .select("name")
+            .eq("id", serviceData.event_id)
+            .single();
+
+          if (eventError || !eventData) {
+            console.error("Error fetching related event data:", eventError);
+          } else {
+            setRelatedEventName(eventData.name);
+          }
         }
+
         setLoading(false); // ここでローディング終了
         console.log(serviceData);
       }
     };
-
     fetchServiceData();
   }, [serviceID]);
 
@@ -63,14 +74,49 @@ export default function ServiceDetailPage({
 
   return (
     <main className={styles.fullScreen}>
-      <div className={styles.container}>
-        <p className={styles.description}>{service.comment}</p>
-        <p className={styles.details}>開催日: {service.create_date}</p>
-        <p className={styles.details}>説明: {service.description}</p>
-        <p className={styles.details}>チーム: {service.team_name}</p>
+      {/* <div className={styles.Header}></div> */}
 
-        {/* 賞のリストを表示 */}
-        {/* {service.awards && service.awards.length > 0 && (
+      <div className={styles.head}>
+        <h1 className={styles.servicename}>
+          サービス詳細 <h2 className={styles.span}> ＞ {service.name}</h2>
+        </h1>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.container}>
+          <div className={styles.atama}>
+            <h3 className={styles.title}>{service.name}</h3>
+            <p className={styles.description}>{service.comment}</p>
+          </div>
+
+          <div className={styles.karada}>
+            {service.image ? (
+              <img
+                className={styles.image}
+                src={service.image}
+                alt={service.name}
+              />
+            ) : (
+              <p>画像がありません</p> // 画像が存在しない場合のフォールバック
+            )}
+            <div className={styles.detailsContainer}>
+              <p className={styles.details}>
+                作成月: {service.release_year}/{service.release_month}
+              </p>
+              {/* <p className={styles.details}>説明: {service.description}</p> */}
+              <p className={styles.details}>チーム名: {service.team_name}</p>
+              {/* 関連イベントを表示 */}
+              {relatedEventName ? (
+                <p className={styles.details}>
+                  関連イベント: {relatedEventName}
+                </p>
+              ) : (
+                <p>関連イベントがありません</p>
+              )}{" "}
+            </div>
+          </div>
+
+          {/* 賞のリストを表示 */}
+          {/* {service.awards && service.awards.length > 0 && (
           <div className={styles.awardsContainer}>
             <h2 className={styles.title}>賞一覧</h2>
             {service.awards.map((award: Award, index: number) => (
@@ -80,6 +126,7 @@ export default function ServiceDetailPage({
             ))}
           </div>
         )} */}
+        </div>
       </div>
     </main>
   );
--- src/types/Service.ts
+++ src/types/Service.ts
@@ -1,4 +1,5 @@
 import { Award } from "@/types/Award";
+import { ReactNode } from "react";
 
 //Service一覧ページで利用する型定義
 export interface Service {
@@ -9,6 +10,9 @@ export interface Service {
 
 //Service詳細ページで利用する型定義
 export interface ServiceDetail extends Service {
+  release_month: ReactNode;
+  release_year: ReactNode;
+  image: any;
   description?: string;
   team_name: string;
   create_date?: string;
