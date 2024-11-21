3f9297378268ba4c3d3312e95e88ac44c0d1a5ad
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-11-21 13:41:07 +0900

    userの全deta取得完了

--- src/app/admin/users/existing-users/[user_id]/edit/page.tsx
+++ src/app/admin/users/existing-users/[user_id]/edit/page.tsx
@@ -9,7 +9,6 @@ import FormButton from '@/components/form/FormButton';
 import { supabase } from '@/supabase/supabase';
 import AdminHeader from '@/components/admin/AdminHeader';
 import LoadingModal from '@/components/loading/LoadingModal';
-import { get } from 'http';
 
 type userTableData = {
     name: string;
@@ -23,14 +22,7 @@ type userTableData = {
 type userTechsData = number;
 
 type snsTableData = {
-    x_id: string;
-    instagram_id: string;
-    github_id: string;
-}
-
-type tecksTableData = {
-    id: number;
-    name: string;
+    id: string;
 }
 
 const EditServicesPage = ({
@@ -93,19 +85,19 @@ const EditServicesPage = ({
                     return undefined;
                 }
 
-                let file = undefined;
+                // let file = undefined;
 
-                if(userData.image){
-                    const url = await fetch(userData.image);
-                    const blob = await url.blob();
-                    file = new File([blob], userData.image);
-                }
+                // if(userData.image){
+                //     const url = await fetch(userData.image);
+                //     const blob = await url.blob();
+                //     file = new File([blob], userData.image);
+                // }
                 return {
                     name: userData.name,
                     nickname: userData.nickname,
                     account_id: userData.account_id,
                     introduction: userData.introduction,
-                    image: file,
+                    image: undefined,
                     imageURL: userData.image,
                 };
             }
@@ -129,11 +121,62 @@ const EditServicesPage = ({
                 return [];
             }
         };
+        const fetchUserXData = async () => {
+            try {
+                const { data: getUserXData, error: xError } = await supabase
+                    .from('x')
+                    .select("x_id")
+                    .eq("user_id", userID)
+                    .single();
+    
+                if (xError) {
+                    throw new Error(`Error fetching techs: ${xError.message}`);
+                }
+                return {id: getUserXData.x_id};
+            } catch (error) {
+                console.error(error);
+                return undefined;
+            }
+        };
+        const fetchUserInstagramData = async () => {
+            try {
+                const { data: getUserInstagramData, error: instagramError } = await supabase
+                    .from('instagram')
+                    .select("instagram_id")
+                    .eq("user_id", userID)
+                    .single();
+    
+                if (instagramError) {
+                    throw new Error(`Error fetching techs: ${instagramError.message}`);
+                }
+                return {id: getUserInstagramData.instagram_id};
+            } catch (error) {
+                console.error(error);
+                return undefined;
+            }
+        };
+        const fetchUserGithubData = async () => {
+            try {
+                const { data: getUserGithubData, error: githubError } = await supabase
+                    .from('github')
+                    .select("github_id")
+                    .eq("user_id", userID)
+                    .single();
+    
+                if (githubError) {
+                    throw new Error(`Error fetching techs: ${githubError.message}`);
+                }
+                return {id: getUserGithubData.github_id};
+            } catch (error) {
+                console.error(error);
+                return undefined;
+            }
+        };
     
         const fetchData = async () => {
             setIsLoading(true);
             try {
-                const [tecksData, userTableData, userTechsData]: [teckData[] | undefined, userTableData | undefined, userTechsData[] | []] = await Promise.all([fetchTecksData(), fetchUserData(), fetchUserTecksData()]);
+                const [tecksData, userTableData, userTechsData, userXData, userInstagramData, userGithubData]: [teckData[] | undefined, userTableData | undefined, userTechsData[] | [], snsTableData| undefined, snsTableData| undefined, snsTableData| undefined ] = await Promise.all([fetchTecksData(), fetchUserData(), fetchUserTecksData(), fetchUserXData(), fetchUserInstagramData(), fetchUserGithubData()]);
                 
                 reset({
                     iconImage: userTableData?.image,
@@ -142,9 +185,9 @@ const EditServicesPage = ({
                     account_id: userTableData?.account_id,
                     introduction: userTableData?.introduction,
                     technologiesId: userTechsData,
-                    x_id: "",
-                    instagram_id: "",
-                    github_id: "",
+                    x_id: userXData?.id,
+                    instagram_id: userInstagramData?.id,
+                    github_id: userGithubData?.id,
                   });
 
                 setFormFields(useFormFields(control, tecksData, userTableData?.imageURL));
@@ -165,100 +208,102 @@ const EditServicesPage = ({
 
     const onSubmit: SubmitHandler<ServiceOutputSchema> = async (data) => {
         setIsLoading(true);
+        console.log("hogehofe");
+        console.log(data);
 
-        let imageUrl = '';
+        // let imageUrl = '';
 
-        if (data.iconImage instanceof File) {
-            const imageFileName = encodeURIComponent(`${Date.now()}-${data.iconImage.name.replace(/[^a-zA-Z0-9.]/g, '_')}`);
-            const { error: uploadError } = await supabase.storage
-                .from('user_icons')
-                .upload(imageFileName, data.iconImage);
+        // if (data.iconImage instanceof File) {
+        //     const imageFileName = encodeURIComponent(`${Date.now()}-${data.iconImage.name.replace(/[^a-zA-Z0-9.]/g, '_')}`);
+        //     const { error: uploadError } = await supabase.storage
+        //         .from('user_icons')
+        //         .upload(imageFileName, data.iconImage);
 
-            if (uploadError) {
-                console.error('Error uploading file: ', uploadError.message);
-                window.alert(uploadError.message);
-                setIsLoading(false);
-                return;
-            }
+        //     if (uploadError) {
+        //         console.error('Error uploading file: ', uploadError.message);
+        //         window.alert(uploadError.message);
+        //         setIsLoading(false);
+        //         return;
+        //     }
 
-            const { data: publicImageUrlData } = await supabase.storage
-                .from('user_icons')
-                .getPublicUrl(imageFileName);
+        //     const { data: publicImageUrlData } = await supabase.storage
+        //         .from('user_icons')
+        //         .getPublicUrl(imageFileName);
 
-            imageUrl = publicImageUrlData.publicUrl || '';
-        }
+        //     imageUrl = publicImageUrlData.publicUrl || '';
+        // }
 
-        const { iconImage, technologiesId, x_id, instagram_id, github_id, ...rest } = data;
-        const submitData = { ...rest, image: imageUrl };
+        // const { iconImage, technologiesId, x_id, instagram_id, github_id, ...rest } = data;
+        // const submitData = { ...rest, image: imageUrl };
 
-        const { data: userData, error: insertError } = await supabase
-            .from('users')
-            .insert([submitData])
-            .select();
+        // const { data: userData, error: insertError } = await supabase
+        //     .from('users')
+        //     .insert([submitData])
+        //     .select();
 
-        if (insertError) {
-            console.error('Error inserting service:', insertError);
-            window.alert(insertError.message);
-            setIsLoading(false);
-            return;
-        }
+        // if (insertError) {
+        //     console.error('Error inserting service:', insertError);
+        //     window.alert(insertError.message);
+        //     setIsLoading(false);
+        //     return;
+        // }
 
-        if (data.technologiesId && data.technologiesId.length > 0) {
-            const { error: technologiesError } = await supabase
-                .from('users_technologies')
-                .insert(data.technologiesId.map((technologyId) => ({
-                    user_id: userData[0].id,
-                    technology_id: technologyId,
-                })));
+        // if (data.technologiesId && data.technologiesId.length > 0) {
+        //     const { error: technologiesError } = await supabase
+        //         .from('users_technologies')
+        //         .insert(data.technologiesId.map((technologyId) => ({
+        //             user_id: userData[0].id,
+        //             technology_id: technologyId,
+        //         })));
 
-            if (technologiesError) {
-                console.error('Error inserting technologies:', technologiesError);
-                window.alert(technologiesError.message);
-                setIsLoading(false);
-                return;
-            }
-        }
+        //     if (technologiesError) {
+        //         console.error('Error inserting technologies:', technologiesError);
+        //         window.alert(technologiesError.message);
+        //         setIsLoading(false);
+        //         return;
+        //     }
+        // }
 
 
-        if (x_id) {
-            const { error: xIdError } = await supabase
-                .from('x')
-                .insert([{ user_id: userData[0].id, x_id: x_id }]);
+        // if (x_id) {
+        //     const { error: xIdError } = await supabase
+        //         .from('x')
+        //         .insert([{ user_id: userData[0].id, x_id: x_id }]);
 
-            if (xIdError) {
-                console.error('Error inserting url_web:', xIdError);
-                window.alert(xIdError.message);
-                setIsLoading(false);
-                return;
-            }
-        }
+        //     if (xIdError) {
+        //         console.error('Error inserting url_web:', xIdError);
+        //         window.alert(xIdError.message);
+        //         setIsLoading(false);
+        //         return;
+        //     }
+        // }
 
-        if (instagram_id) {
-            const { error: instagramIdError } = await supabase
-                .from('instagram')
-                .insert([{ user_id: userData[0].id, instagram_id: instagram_id }]);
+        // if (instagram_id) {
+        //     const { error: instagramIdError } = await supabase
+        //         .from('instagram')
+        //         .insert([{ user_id: userData[0].id, instagram_id: instagram_id }]);
 
-            if (instagramIdError) {
-                console.error('Error inserting url_web:', instagramIdError);
-                window.alert(instagramIdError.message);
-                setIsLoading(false);
-                return;
-            }
-        }
+        //     if (instagramIdError) {
+        //         console.error('Error inserting url_web:', instagramIdError);
+        //         window.alert(instagramIdError.message);
+        //         setIsLoading(false);
+        //         return;
+        //     }
+        // }
 
-        if (github_id) {
-            const { error: githubIdError } = await supabase
-                .from('github')
-                .insert([{ user_id: userData[0].id, github_id: github_id }]);
+        // if (github_id) {
+        //     const { error: githubIdError } = await supabase
+        //         .from('github')
+        //         .insert([{ user_id: userData[0].id, github_id: github_id }]);
 
-            if (githubIdError) {
-                console.error('Error inserting url_web:', githubIdError);
-                window.alert(githubIdError.message);
-                setIsLoading(false);
-                return;
-            }
-        }
-        router.push('/admin/users/existing-users');
+        //     if (githubIdError) {
+        //         console.error('Error inserting url_web:', githubIdError);
+        //         window.alert(githubIdError.message);
+        //         setIsLoading(false);
+        //         return;
+        //     }
+        // }
+        // router.push('/admin/users/existing-users');
     };
 
     return (
