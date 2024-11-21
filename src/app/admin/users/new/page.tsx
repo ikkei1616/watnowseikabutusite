c67ba35d0c64030d14d6c1d25880e1087896286c
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-11-21 18:02:56 +0900

    ローディング変更

--- src/app/admin/users/new/page.tsx
+++ src/app/admin/users/new/page.tsx
@@ -18,37 +18,38 @@ const NewServicesPage = () => {
   });
   const [formFields, setFormFields] = useState<{ container: string, title: string, fields: FormField<ServiceInputSchema>[] }[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isFirstLoading, setIsFirstLoading] = useState(true);
 
   const router = useRouter();
 
 
   useEffect(() => {
-      const fetchData = async () => {
-          try {
-              const { data: techsData, error: techsError } = await supabase
-                  .from('technologies')
-                  .select('id, name')
+    const fetchData = async () => {
+      try {
+        const { data: techsData, error: techsError } = await supabase
+          .from('technologies')
+          .select('id, name')
 
-              if (techsError) {
-                  throw new Error(`Error fetching techs: ${techsError.message}`);
-              }
-              setFormFields(useFormFields(control,techsData.map((tech) => ({ value: tech.id, label: tech.name })) || []));
+        if (techsError) {
+          throw new Error(`Error fetching techs: ${techsError.message}`);
+        }
+        setFormFields(useFormFields(control, techsData.map((tech) => ({ value: tech.id, label: tech.name })) || []));
+        setIsFirstLoading(false);
 
-          } catch (error) {
-              console.error(error);
-          }
-      };
-      setIsLoading(true);
-      fetchData();
-      setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const handleCancel = () => {
     const confirmDelete = window.confirm("入力内容が破棄されますがよろしいですか？");
 
-        if (!confirmDelete) {
-            return;
-        }
+    if (!confirmDelete) {
+      return;
+    }
     router.push('/admin/users');
   }
 
@@ -77,7 +78,7 @@ const NewServicesPage = () => {
       imageUrl = publicImageUrlData.publicUrl || '';
     }
 
-    const {iconImage, technologiesId, x_id, instagram_id, github_id, ...rest } = data;
+    const { iconImage, technologiesId, x_id, instagram_id, github_id, ...rest } = data;
     const submitData = { ...rest, image: imageUrl };
 
     const { data: userData, error: insertError } = await supabase
@@ -108,45 +109,45 @@ const NewServicesPage = () => {
       }
     }
 
-    
-    if(x_id){
-        const { error: xIdError } = await supabase
-          .from('x')
-          .insert([{user_id: userData[0].id, x_id: x_id}]);
-  
-        if (xIdError) {
-          console.error('Error inserting url_web:', xIdError);
-          window.alert(xIdError.message);
-          setIsLoading(false);
-          return;
-        }
-      }
 
-    if(instagram_id){
-        const { error: instagramIdError } = await supabase
-          .from('instagram')
-          .insert([{user_id: userData[0].id, instagram_id: instagram_id}]);
-  
-        if (instagramIdError) {
-          console.error('Error inserting url_web:', instagramIdError);
-          window.alert(instagramIdError.message);
-          setIsLoading(false);
-          return;
-        }
-      }
+    if (x_id) {
+      const { error: xIdError } = await supabase
+        .from('x')
+        .insert([{ user_id: userData[0].id, x_id: x_id }]);
 
-    if(github_id){
-        const { error: githubIdError } = await supabase
-          .from('github')
-          .insert([{user_id: userData[0].id, github_id: github_id}]);
-  
-        if (githubIdError) {
-          console.error('Error inserting url_web:', githubIdError);
-          window.alert(githubIdError.message);
-          setIsLoading(false);
-          return;
-        }
+      if (xIdError) {
+        console.error('Error inserting url_web:', xIdError);
+        window.alert(xIdError.message);
+        setIsLoading(false);
+        return;
       }
+    }
+
+    if (instagram_id) {
+      const { error: instagramIdError } = await supabase
+        .from('instagram')
+        .insert([{ user_id: userData[0].id, instagram_id: instagram_id }]);
+
+      if (instagramIdError) {
+        console.error('Error inserting url_web:', instagramIdError);
+        window.alert(instagramIdError.message);
+        setIsLoading(false);
+        return;
+      }
+    }
+
+    if (github_id) {
+      const { error: githubIdError } = await supabase
+        .from('github')
+        .insert([{ user_id: userData[0].id, github_id: github_id }]);
+
+      if (githubIdError) {
+        console.error('Error inserting url_web:', githubIdError);
+        window.alert(githubIdError.message);
+        setIsLoading(false);
+        return;
+      }
+    }
     router.push('/admin/users/existing-users');
   };
 
@@ -158,35 +159,37 @@ const NewServicesPage = () => {
         margin: "0 auto",
       }
       }>
-        <AdminHeader isEditing/>
-        <h1 style={{
-          borderBottom: "1px solid #9CABC7",
-          paddingBottom: "12px",
-          marginBottom: "12px",
-        }}>新規ユーザ作成</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {formFields.map(({ title, fields }, index) => (
-            <section key={index} style={{
-              borderBottom: "1px solid #9CABC7",
-              paddingBottom: "12px",
-              marginBottom: "12px",
+        <AdminHeader isEditing />
+        {!isFirstLoading && <div>
+          <h1 style={{
+            borderBottom: "1px solid #9CABC7",
+            paddingBottom: "12px",
+            marginBottom: "12px",
+          }}>新規ユーザ作成</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {formFields.map(({ title, fields }, index) => (
+              <section key={index} style={{
+                borderBottom: "1px solid #9CABC7",
+                paddingBottom: "12px",
+                marginBottom: "12px",
+              }}>
+                <h3>{title}</h3>
+                {fields.map((field) => (
+                  <FormFactory<ServiceInputSchema> key={field.id} {...field} />
+                ))}
+              </section>
+            ))}
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              gap: '60px',
+              margin: "20px 0"
             }}>
-              <h3>{title}</h3>
-              {fields.map((field) => (
-                <FormFactory<ServiceInputSchema> key={field.id} {...field} />
-              ))}
-            </section>
-          ))}
-          <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '60px',
-            margin: "20px 0"
-          }}>
-            <FormButton name="キャンセル" type='cancel' onClick={handleCancel}/>
-            <FormButton name="新規作成" type='submit' />
-          </div>
-        </form>
+              <FormButton name="キャンセル" type='cancel' onClick={handleCancel} />
+              <FormButton name="新規作成" type='submit' />
+            </div>
+          </form>
+        </div>}
       </main>
     </>
   );
