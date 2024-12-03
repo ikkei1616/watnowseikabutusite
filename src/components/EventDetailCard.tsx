a526a7733c6f5f272f0598c9d0a61c70658f405b
Taichi <takezohuto1123@icloud.com>
2024-12-03 13:28:14 +0900

    fix: イベント関連URLを表示できるようにした

--- src/components/EventDetailCard.tsx
+++ src/components/EventDetailCard.tsx
@@ -155,16 +155,16 @@ const EventDetailCard: React.FC<EventDetailCardProps> = ({
             関連情報
           </Typography>
           <DottedDivider color="#878686" />
-          {event.url ? (
-            <Link href={event.url}>イベントURLはこちら</Link>
-          ) : (
-            <Typography
-              variant="body1"
-              sx={{ color: "#878686", fontSize: "1rem", marginTop: "0.3rem" }}
-            >
-              登録なし
-            </Typography>
-          )}
+          <Typography
+            variant="body1"
+            sx={{ color: "#878686", fontSize: "1rem", marginTop: "0.3rem" }}
+          >
+            {event.url ? (
+              <Link href={event.url}>{event.url}</Link>
+            ) : (
+              <Typography>登録なし</Typography>
+            )}
+          </Typography>
         </Box>
       </Box>
       <Box
