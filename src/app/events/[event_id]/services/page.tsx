bf6f631e90b719910a6298dc1252c2b9ac5d19c1
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-01-30 17:09:28 +0900

    イベントがない場合のバリデーション

--- src/app/events/[event_id]/services/page.tsx
+++ src/app/events/[event_id]/services/page.tsx
@@ -7,12 +7,14 @@ import EventHeader from "@/components/EventHeader";
 import ServicesContainer from "@/components/ServicesContainer";
 import { EventAllService } from "@/types/Service";
 import { HeaderMode } from "@/types/HeaderMode";
-
+import { Box, Typography } from "@mui/material";
+import styles from "./page.module.css";
 
 
 
 const EventServices = ({ params }: { params: { event_id: string } }) => {
   const [eventId] = useState<string>(params.event_id);
+  const [isEvent, setIsEvent] = useState(true);
   const [eventName, setEventName] = useState<string>("");
   const [test, setTest] = useState<EventAllService[]>([]);
 
@@ -22,6 +24,18 @@ const EventServices = ({ params }: { params: { event_id: string } }) => {
         console.log("eventID error", eventId);
         return;
       }
+
+      const { data:eventData, error:eventError } = await supabase
+        .from("events")
+        .select("*")
+        .eq("id", eventId);
+      
+        if (eventError) {
+          console.log("Error fetching event data:", eventError?.message);
+          setIsEvent(false)
+          return;
+        }
+
 
       const { data, error } = await supabase
         .from("services")
@@ -48,12 +62,34 @@ const EventServices = ({ params }: { params: { event_id: string } }) => {
         return;
       }
       // setEventName(eventName as EventName[] || [])
-      setEventName(eventName[0].name || "");
+      if (eventName && eventName.length > 0) {
+        setEventName(eventName[0].name || "");
+      }
     };
 
     fetchEventName();
     fetchData();
   }, [eventId]);
+
+  if (isEvent) {
+    return (
+      <>
+        <Header mode={HeaderMode.SERVICES}></Header>
+        <Box
+          sx={{
+            width:"100%",
+            height:"100vh",
+            display:"grid",
+            placeItems:"center",
+          }}
+        >
+          <p className={styles.notFound}>イベントが見つかりませんでした！</p>
+        </Box> 
+      </>
+      
+    )
+  }
+
 
   return (
     <>
