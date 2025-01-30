833c993ad9c280282ee61b90bfc371ba0d5a1bdd
Taichi <takezohuto1123@icloud.com>
2025-01-30 17:04:50 +0900

    fix: 余白の調整

--- src/components/EventDetailCard.tsx
+++ src/components/EventDetailCard.tsx
@@ -25,278 +25,283 @@ const EventDetailCard: React.FC<EventDetailCardProps> = ({
   //   <DottedDivider color="#878686" />
 
   return (
-    <Card
+    <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        width: "85%",
-        maxWidth: "1000px", // 最大幅を設定
-
-        borderRadius: "12px",
-        overflow: "hidden",
-        border: "1px solid",
-        borderColor: "#fff",
-        fontFamily: "HannariMincho", // カスタムフォントを指定
-        margin: "auto",
+        width: "100%",
+        padding: "0 80px 80px 80px",
       }}
     >
-      <Box
+      <Card
         sx={{
           display: "flex",
-          alignItems: "flex-end", // 下方揃え
-          justifyContent: "space-between", // 日付とボタンを左右に配置
-          paddingTop: "0.5rem",
-          paddingBottom: "0",
-          paddingLeft: "1rem",
-          paddingRight: "1rem",
-          position: "relative",
-          fontFamily: "HannariMincho",
-        }}
-      >
-        {/* イベント名を表示 */}
-        <Typography
-          variant="subtitle1"
-          color="#00AEEF"
-          sx={{
-            fontSize: "3rem", // カスタムフォントサイズを指定
-            backgroundColor: "#fff",
-            padding: "0.5rem 1rem", // 上下左右の余白
-            fontFamily: "HannariMincho",
-          }}
-        >
-          {event.name}
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex", //横並びのレイアウト
+          flexDirection: "column",
+          width: "100%",
+
+          borderRadius: "12px",
+          overflow: "hidden",
+          border: "1px solid",
+          borderColor: "#fff",
+          fontFamily: "HannariMincho", // カスタムフォントを指定
         }}
       >
         <Box
           sx={{
-            width: "50%", // 幅を相対的に設定
-            aspectRatio: "16/9", // 画像のアスペクト比を固定
+            display: "flex",
+            alignItems: "flex-end", // 下方揃え
+            justifyContent: "space-between", // 日付とボタンを左右に配置
+            paddingTop: "0.5rem",
+            paddingBottom: "0",
+            paddingLeft: "1rem",
+            paddingRight: "1rem",
             position: "relative",
-            backgroundColor: "##f0f0f0",
-            borderRadius: "8px", // 画像のボーダーを曲げる
-            overflow: "hidden",
-            marginTop: "1rem",
-            marginBottom: "1rem",
-            marginLeft: "2.5rem",
-            marginRight: "2.5rem",
+            fontFamily: "HannariMincho",
           }}
         >
-          {event.image ? (
-            <Image
-              src={event.image}
-              alt={`${event.name}の画像`}
-              layout="fill" // 親のBox全体をカバー
-              objectFit="cover"
-              style={{ borderRadius: "8px" }} // ボーダーの半径を設定
-            />
-          ) : (
-            <Box
-              sx={{
-                backgroundColor: "#D9D9D9",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100%",
-                width: "100%",
-              }}
+          {/* イベント名を表示 */}
+          <Typography
+            variant="subtitle1"
+            color="#00AEEF"
+            sx={{
+              fontSize: "3rem", // カスタムフォントサイズを指定
+              backgroundColor: "#fff",
+              padding: "0.5rem 1rem", // 上下左右の余白
+              fontFamily: "HannariMincho",
+            }}
+          >
+            {event.name}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex", //横並びのレイアウト
+          }}
+        >
+          <Box
+            sx={{
+              width: "50%", // 幅を相対的に設定
+              aspectRatio: "16/9", // 画像のアスペクト比を固定
+              position: "relative",
+              backgroundColor: "##f0f0f0",
+              borderRadius: "8px", // 画像のボーダーを曲げる
+              overflow: "hidden",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+              marginLeft: "2.5rem",
+              marginRight: "2.5rem",
+            }}
+          >
+            {event.image ? (
+              <Image
+                src={event.image}
+                alt={`${event.name}の画像`}
+                layout="fill" // 親のBox全体をカバー
+                objectFit="cover"
+                style={{ borderRadius: "8px" }} // ボーダーの半径を設定
+              />
+            ) : (
+              <Box
+                sx={{
+                  backgroundColor: "#D9D9D9",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                  width: "100%",
+                }}
+              >
+                画像がありません
+              </Box>
+            )}
+          </Box>
+          <Box
+            sx={{
+              marginleft: "1rem",
+              marginRight: "1.5rem",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+              flexGrow: 1,
+            }}
+          >
+            <Typography variant="body1" sx={{ color: "#878686" }}>
+              開催月
+            </Typography>
+            <DottedDivider color="#878686" />
+            <Typography
+              variant="body1"
+              sx={{ color: "#878686", marginTop: "0.3rem", fontSize: "1.3rem" }}
             >
-              画像がありません
-            </Box>
+              {event.year}年{event.month}月
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ color: "#878686", marginTop: "0.7rem" }}
+            >
+              開催場所
+            </Typography>
+            <DottedDivider color="#878686" />
+            <Typography
+              variant="body1"
+              sx={{ color: "#878686", marginTop: "0.3rem", fontSize: "1.3rem" }}
+            >
+              {event.location || "登録なし"}
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ color: "#878686", marginTop: "0.7rem" }}
+            >
+              関連情報
+            </Typography>
+            <DottedDivider color="#878686" />
+            <Typography
+              variant="body1"
+              sx={{ color: "#878686", fontSize: "1rem", marginTop: "0.3rem" }}
+            >
+              {event.url ? (
+                <Link href={event.url}>{event.url}</Link>
+              ) : (
+                <Typography>登録なし</Typography>
+              )}
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            marginLeft: "2rem",
+            marginRight: "1rem",
+            marginTop: "1rem",
+            marginBottom: "1rem",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "1.4rem",
+              color: "#00AEEF",
+              fontFamily: "HannariMincho",
+            }}
+          >
+            イベント詳細
+          </Typography>
+          <DottedDivider color="#00AEEF" />
+          <Box sx={{ marginTop: "1rem" }}>
+            <Typography sx={{ lineHeight: 1.5 }}>
+              {event.comment ? event.comment : "コメントはありません"}
+            </Typography>
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            marginLeft: "2rem",
+            marginRight: "1rem",
+            marginTop: "1rem",
+            marginBottom: "1rem",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "1.4rem",
+              color: "#00AEEF",
+              fontFamily: "HannariMincho",
+            }}
+          >
+            代表サービス
+          </Typography>
+          <DottedDivider color="#00AEEF" />
+        </Box>
+        <Box
+          sx={{
+            display: "flex", // Flexbox レイアウトを有効化
+            flexWrap: "wrap", // 必要に応じて折り返し
+            gap: "16px", // カード間の間隔
+            padding: "16px", // コンテナの内側の余白
+            justifyContent: "flex-start", // 左詰めで表示
+            marginLeft: "1rem",
+          }}
+        >
+          {services?.length ? (
+            <ServicesContainer>
+              {services.map((service) => (
+                <Box
+                  key={service.id}
+                  // sx={{
+                  //   flex: "1 1 calc(33.333% - 16px)", // 幅を3等分（ギャップを考慮）
+                  //   maxWidth: "calc(33.333% - 16px)", // 最大幅を設定
+                  //   boxSizing: "border-box", // パディングとボーダーを含む幅を計算
+                  // }}
+                >
+                  <ServiceCard service={service} />
+                </Box>
+              ))}
+            </ServicesContainer>
+          ) : (
+            <Typography>登録なし</Typography>
           )}
         </Box>
         <Box
           sx={{
-            marginleft: "1rem",
-            marginRight: "1.5rem",
-            marginTop: "1rem",
-            marginBottom: "1rem",
-            flexGrow: 1,
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+            width: "100%", // 全体の幅
+            paddingLeft: "1rem", // 左側の余白
+            paddingRight: "1rem", // 右側の余白
+            paddingBottom: "2rem", // 下側の余白
           }}
         >
-          <Typography variant="body1" sx={{ color: "#878686" }}>
-            開催月
-          </Typography>
-          <DottedDivider color="#878686" />
-          <Typography
-            variant="body1"
-            sx={{ color: "#878686", marginTop: "0.3rem", fontSize: "1.3rem" }}
-          >
-            {event.year}年{event.month}月
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "#878686", marginTop: "0.7rem" }}
-          >
-            開催場所
-          </Typography>
-          <DottedDivider color="#878686" />
-          <Typography
-            variant="body1"
-            sx={{ color: "#878686", marginTop: "0.3rem", fontSize: "1.3rem" }}
-          >
-            {event.location || "登録なし"}
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "#878686", marginTop: "0.7rem" }}
-          >
-            関連情報
-          </Typography>
-          <DottedDivider color="#878686" />
-          <Typography
-            variant="body1"
-            sx={{ color: "#878686", fontSize: "1rem", marginTop: "0.3rem" }}
-          >
-            {event.url ? (
-              <Link href={event.url}>{event.url}</Link>
-            ) : (
-              <Typography>登録なし</Typography>
-            )}
-          </Typography>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          marginLeft: "2rem",
-          marginRight: "1rem",
-          marginTop: "1rem",
-          marginBottom: "1rem",
-        }}
-      >
-        <Typography
-          sx={{
-            fontSize: "1.4rem",
-            color: "#00AEEF",
-            fontFamily: "HannariMincho",
-          }}
-        >
-          イベント詳細
-        </Typography>
-        <DottedDivider color="#00AEEF" />
-        <Box sx={{ marginTop: "1rem" }}>
-          <Typography sx={{ lineHeight: 1.5 }}>
-            {event.comment ? event.comment : "コメントはありません"}
-          </Typography>
-        </Box>
-      </Box>
-
-      <Box
-        sx={{
-          marginLeft: "2rem",
-          marginRight: "1rem",
-          marginTop: "1rem",
-          marginBottom: "1rem",
-        }}
-      >
-        <Typography
-          sx={{
-            fontSize: "1.4rem",
-            color: "#00AEEF",
-            fontFamily: "HannariMincho",
-          }}
-        >
-          代表サービス
-        </Typography>
-        <DottedDivider color="#00AEEF" />
-      </Box>
-      <Box
-        sx={{
-          display: "flex", // Flexbox レイアウトを有効化
-          flexWrap: "wrap", // 必要に応じて折り返し
-          gap: "16px", // カード間の間隔
-          padding: "16px", // コンテナの内側の余白
-          justifyContent: "flex-start", // 左詰めで表示
-          marginLeft: "1rem",
-        }}
-      >
-        {services?.length ? (
-          <ServicesContainer>
-            {services.map((service) => (
-              <Box
-                key={service.id}
-                // sx={{
-                //   flex: "1 1 calc(33.333% - 16px)", // 幅を3等分（ギャップを考慮）
-                //   maxWidth: "calc(33.333% - 16px)", // 最大幅を設定
-                //   boxSizing: "border-box", // パディングとボーダーを含む幅を計算
-                // }}
-              >
-                <ServiceCard service={service} />
-              </Box>
-            ))}
-          </ServicesContainer>
-        ) : (
-          <Typography>登録なし</Typography>
-        )}
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: "8px",
-          width: "100%", // 全体の幅
-          paddingLeft: "1rem", // 左側の余白
-          paddingRight: "1rem", // 右側の余白
-          paddingBottom: "2rem", // 下側の余白
-        }}
-      >
-        {/* 左側の線 */}
-        <Divider
-          sx={{
-            flexGrow: 1, // 残りのスペースを埋める
-            borderColor: "#00AEEF", // 線の色
-            borderWidth: "1px", // 線の太さ
-          }}
-        />
-
-        <Link href={`/events/${event.id}/services`} passHref>
-          <Button
-            size="small"
-            color="primary"
+          {/* 左側の線 */}
+          <Divider
             sx={{
-              textTransform: "none",
-              "&:hover": {
-                backgroundColor: "#D9D9D9", // ホバー時の背景色を濃くする
-              },
-              fontFamily: "HannariMincho",
+              flexGrow: 1, // 残りのスペースを埋める
+              borderColor: "#00AEEF", // 線の色
+              borderWidth: "1px", // 線の太さ
             }}
-          >
-            <Typography
+          />
+
+          <Link href={`/events/${event.id}/services`} passHref>
+            <Button
+              size="small"
+              color="primary"
               sx={{
-                whiteSpace: "nowrap", // テキストを折り返さない
-                fontSize: "16px", // フォントサイズ
-                color: "#4D4D4D", // テキストの色
-                display: "flex", // テキストと画像を横並び
-                alignItems: "center",
-                gap: "8px", // テキストと画像の間隔
+                textTransform: "none",
+                "&:hover": {
+                  backgroundColor: "#D9D9D9", // ホバー時の背景色を濃くする
+                },
+                fontFamily: "HannariMincho",
               }}
             >
-              すべてのプロダクトを見る
-              {/* 画像を挿入 */}
-              <Image
-                src={"/paper_airplane_blue.svg"}
-                alt={"紙飛行機のアイコン"}
-                height={20}
-                width={20}
-              />
-            </Typography>
-          </Button>
-        </Link>
+              <Typography
+                sx={{
+                  whiteSpace: "nowrap", // テキストを折り返さない
+                  fontSize: "16px", // フォントサイズ
+                  color: "#4D4D4D", // テキストの色
+                  display: "flex", // テキストと画像を横並び
+                  alignItems: "center",
+                  gap: "8px", // テキストと画像の間隔
+                }}
+              >
+                すべてのプロダクトを見る
+                {/* 画像を挿入 */}
+                <Image
+                  src={"/paper_airplane_blue.svg"}
+                  alt={"紙飛行機のアイコン"}
+                  height={20}
+                  width={20}
+                />
+              </Typography>
+            </Button>
+          </Link>
 
-        {/* 右側の線 */}
-        <Divider
-          sx={{
-            flexGrow: 1, // 残りのスペースを埋める
-            borderColor: "#00AEEF", // 線の色
-            borderWidth: "1px", // 線の太さ
-          }}
-        />
-      </Box>
-    </Card>
+          {/* 右側の線 */}
+          <Divider
+            sx={{
+              flexGrow: 1, // 残りのスペースを埋める
+              borderColor: "#00AEEF", // 線の色
+              borderWidth: "1px", // 線の太さ
+            }}
+          />
+        </Box>
+      </Card>
+    </Box>
   );
 };
 
