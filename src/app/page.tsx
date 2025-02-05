1d70cb322fd2cc1823be8d26a95b28d9ba35d5e3
YukaChoco <sugar.ore.yk@gmail.com>
2025-02-05 14:57:48 +0900

    いらないimport削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import SplashScreen from "@/components/SplashScreen";
-import useSplashScreen from "@/hooks/useSplashScreen";
 
 export default function Home() {
-
   return <SplashScreen />;
 }
